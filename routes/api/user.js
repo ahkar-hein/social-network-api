765fa85746628ce9b9a5f818974803a2751dddcf
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-24 22:01:35 -0700

    implement the user and friends routes at /routes/api/user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -11,5 +11,7 @@ const {
 } = require('../../controller/userController')
 
 router.route('/').get(getUsers).post(createUser);
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendsId').post(createFriend).delete(deleteFriend);
 
 module.exports = router;
