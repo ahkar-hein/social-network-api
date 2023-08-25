9be0c85f5081b896336adb99aea79cacb7ba90b9
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-24 19:53:35 -0700

    create and update routes for user

--- controller/userController.js
+++ controller/userController.js
@@ -11,17 +11,37 @@ module.exports = {
     },
     async getSingleUser(req, res) {
         try {
-          const user = await User.findOne({ _id: req.params.userId })
-            .select('-__v');
-    
-          if (!user) {
-            return res.status(404).json({ message: 'No user with that ID' });
-          }
-    
-          res.json(user);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      },
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
 
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const newUser = await User.create(req.body);
+            res.json(newUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+                runValidators: true
+            });
+            if (!updateUser) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(updateUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 }
