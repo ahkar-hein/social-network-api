c6ac57bffc259d44a7708e34e116aced026b6825
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-24 21:55:59 -0700

    create delete route function for reaction

--- controller/reactionController.js
+++ controller/reactionController.js
@@ -18,5 +18,23 @@ module.exports = {
             res.status(400).json(err);
           }
     },
-    
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findById(req.params.thoughtId);
+        
+            if (!thought) {
+              return res.status(404).json({ message: 'Thought not found' });
+            }
+        
+            thought.reactions.pull(req.params.reactionId);
+            await thought.save();
+        
+            await Reaction.findByIdAndDelete(req.params.reactionId);
+        
+            res.json(thought);
+          } catch (err) {
+            res.status(400).json(err);
+          }
+    }
+
 }
