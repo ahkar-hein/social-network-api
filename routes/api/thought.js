c8e0d4a90593aa2654d1c9e066fb954848b03cfc
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-24 22:07:32 -0700

    implement routes for thought and reaction

---
+++ routes/api/thought.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createNewThought,
+    updateThought,
+    deleteThought,
+} = require('../../controller/thoughtController');
+
+const {
+    createReaction,
+    deleteReaction,
+} = require('../../controller/reactionController')
+
+router.route('/').get(getThought).post(createNewThought);
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
